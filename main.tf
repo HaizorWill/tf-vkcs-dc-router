050e691e74b7683dbd5eee7829fcbc2b92ad046b
HaizorWill <thelines6@gmail.com>
2026-02-10 21:12:47 +0300

    Fix minor mistakes; Add bgp_instance resource; Remove depends_on as of now
    This whole thing needs a rework, because - locals... Too lazy. Remove
    depends_on since i am a dumbass and have not yet understood whether
    terraform knows who depends on whom by just looking at resource
    references. Might want to bring these back in the future

--- main.tf
+++ main.tf
@@ -16,18 +16,39 @@ resource "vkcs_dc_interface" "this" {
   subnet_id            = each.value.subnet_id
   ip_address           = each.value.ip_address
   bgp_announce_enabled = each.value.bgp_announce_enabled
-
-  depends_on = [vkcs_dc_router.this]
 }
 
 resource "vkcs_dc_static_route" "this" {
-  for_each = { for r in var.routes : r.name => r }
+  for_each = { for r in var.routes : r.name => r if var.static_routes }
 
   name         = each.value.name
   description  = each.value.description
   region       = var.region
   dc_router_id = vkcs_dc_router.this.id
-  network      = each.value.name
+  network      = each.value.network
   gateway      = each.value.gateway
   metric       = each.value.metric
 }
+
+resource "vkcs_dc_bgp_instance" "this" {
+  for_each = { for i in var.bgp_instances : i.name => i if var.bgp_enabled }
+
+  name                        = each.value.name
+  description                 = each.value.description
+  region                      = var.region
+  dc_router_id                = vkcs_dc_router.this.id
+  enabled                     = each.value.enabled
+  bgp_router_id               = each.value.bgp_router_id
+  asn                         = each.value.asn
+  ecmp_enabled                = each.value.ecmp_enabled
+  graceful_restart            = each.value.graceful_restart
+  long_lived_graceful_restart = each.value.long_lived_graceful_restart
+}
+
+# resource "vkcs_dc_bgp_neighbor" "this" {
+#   for_each = { for n in var.bgp_neighbors : n.name => n if var.bgp_enabled }
+#
+#   name        = each.value.name
+#   description = each.value.description
+#   region      = var.region
+# }
