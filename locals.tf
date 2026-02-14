00f7e48e7e9bbc752c2df4668a2fe2b88e7e887b
HaizorWill <thelines6@gmail.com>
2026-02-14 12:49:40 +0300

    Add interfaces and routes locals
    Not ready yet, what are you staring at

--- locals.tf
+++ locals.tf
@@ -1,3 +1,16 @@
 locals {
-
+  interfaces = [
+    for id, interface in var.interfaces : merge(interface, {
+      interface_id = id
+      resource_key = coalesce(interface.resource_key, interface.name, format("iface-%s", coalesce(interface.ip_address, id)))
+      }
+    )
+  ]
+  routes = [
+    for id, route in var.routes : merge(route, {
+      route_id     = id
+      resource_key = coalesce(route.resource_key, route.name, format("route-%s", coalesce(route.gateway, id)))
+      }
+    )
+  ]
 }
