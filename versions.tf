5e6c64accf31d9b51b43dbf12a1046763dfeda6b
HaizorWill <thelines6@gmail.com>
2026-02-23 23:43:40 +0300

    Fix ruquired versions

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 0.14.0"
+  required_version = ">= 1.5"
   required_providers {
     vkcs = {
       source  = "vk-cs/vkcs"
-      version = "< 1.0.0"
+      version = ">= 0.8.0, < 1.0.0"
     }
   }
 }
